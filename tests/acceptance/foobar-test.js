63672688e675d5b3e02499206bcb09d5b7d370fc
Tobias Bieniek <tobias.bieniek@gmail.com>
2018-09-06 17:44:31 +0200

    Add percySnapshot() call

--- tests/acceptance/foobar-test.js
+++ tests/acceptance/foobar-test.js
@@ -1,12 +1,14 @@
 import { module, test } from 'qunit';
 import { visit, currentURL } from '@ember/test-helpers';
 import { setupApplicationTest } from 'ember-qunit';
+import { percySnapshot } from 'ember-percy';
 
 module('Acceptance | foobar', function(hooks) {
   setupApplicationTest(hooks);
 
   test('visiting /', async function(assert) {
     await visit('/');
+    await percySnapshot(assert);
 
     assert.equal(currentURL(), '/');
   });
